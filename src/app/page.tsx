cf5a166fdf139ee1cd9e9ad9143c84f23e0440f2
rafael_nunes <rafa.nuns17@gmail.com>
2025-02-08 23:04:53 -0300

    Criando e estilizando a tela de Login - Criando rota de Signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
-import styles from "@/app/page.module.scss";
+import styles from "./page.module.scss";
+import logoPrincipal from "../../public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function App() {
-  return(
-    <main>
-      <h1 className={styles.teste}>TESTE TESTE</h1>
-    </main>
+  return (
+    <>
+
+      <div className={styles.containerCentro}>
+        <Image
+          src={logoPrincipal}
+          alt="Logo Pizzaria" />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email"
+              className={styles.input} />
+
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Sua senha"
+              className={styles.input} />
+
+            <button type="submit" className={styles.button}>
+              Acessar
+            </button>
+          </form>
+
+          <Link href="/signup" className={styles.signup}>
+            Registrar minha empresa
+          </Link>
+        </section>
+      </div>
+
+    </>
   );
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+    return (
+        <>
+            <main>
+                <h1>Tela de Cadastro</h1>
+            </main>
+        </>
+    );
+}
